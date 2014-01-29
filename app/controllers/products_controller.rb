cf6fdd9e21b633362569926792722af692ce0c6b
afleisch <afleisch@gmail.com>
2014-01-28 21:33:30 -0800

    added crud methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,13 +8,32 @@ class ProductsController < ApplicationController
   	id = params[:id]
   	@product = Product.find(id)
   	@categories = @product.categories
+  end
 
-
+  def new
+    @product = Product.new
+    render :new
   end
 
   def create
+  	product = params[:product].permit(:name)
+  	Product.create(product)
+  	redirect_to "/products"
 
+  end
 
+  def edit
+  	product_id = params[:id]
+  	@product = Product.find(product_id)
+  	render :edit
+  end
+
+  def update
+  	product_id = params[:id]
+  	product = Product.find(product_id)
+  	updated_attributes = params.require(:product).permit(:name)
+  	product.update_attributes(updated_attributes)
+  	redirect_to "/products/"
   end
 
 

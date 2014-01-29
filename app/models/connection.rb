622e0b99ec4b1f079443c0bfc5e9adb8c4a0c40b
afleisch <afleisch@gmail.com>
2014-01-28 23:09:38 -0800

    tried to add method to create new association

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,21 @@
 class Connection < ActiveRecord::Base
   belongs_to :product
   belongs_to :category
+
+
+  # def new
+  # @category = Category.new(params[:category])
+  # product = @category.products.new[:product]
+  # product.save
+  # connection = Connection(category_id: @category.id, product_id: product.id)
+  # connection.save
+
+  # end
+
+  # def create
+  # 	product = @category.products.create(params[:category])
+  # 	connection = @category.connections.where(product: product.id).first
+  	
+
+  # end
 end

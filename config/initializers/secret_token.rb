2f3b0aae9a82b3be2e32c8a1138900b8c13767aa
afleisch <afleisch@gmail.com>
2014-01-28 16:36:23 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'de20fc2c990c0d427fb7160af8ae29988371e683d6b38d744bfd1e617f63a60a16181970e218eb182404d24f096ef6588e48f626f887e45bf4d4efe2f1b7ec49'
---
+++ db/seeds.rb
@@ -0,0 +1,54 @@
+categories = []
+categories << Category.create(name: "Dining Room")
+categories << Category.create(name: "Living Room")
+categories << Category.create(name: "Bedroom")
+categories << Category.create(name: "Kitchen")
+categories << Category.create(name: "Office")
+categories << Category.create(name: "Bathroom")
+
+
+products = []
+products << Product.create(name: "Table")
+products << Product.create(name: "Chair")
+products << Product.create(name: "Lamp")
+products << Product.create(name: "Cabinet")
+products << Product.create(name: "Towel")
+products << Product.create(name: "Sofa")
+products << Product.create(name: "Trashcan")
+products << Product.create(name: "TV Stand")
+products << Product.create(name: "Bookcase")
+
+
+# categories[0].products << products[0]
+# categories[0].products << products[1]
+
+# categories[1].products << products[0]
+# categories[1].products << products[1]
+# categories[1].products << products[2]
+# categories[1].products << products[3]
+# categories[1].products << products[5]
+# categories[1].products << products[7]
+# categories[1].products << products[8]
+
+# categories[2].products << products[0]
+# categories[2].products << products[2]
+# categories[2].products << products[3]
+# categories[2].products << products[5]
+# categories[2].products << products[7]
+# categories[2].products << products[8]
+
+# categories[3].products << products[0]
+# categories[3].products << products[1]
+# categories[3].products << products[3]
+# categories[3].products << products[4]
+# categories[3].products << products[6]
+# categories[3].products << products[7]
+
+# categories[4].products << products[1]
+# categories[4].products << products[2]
+# categories[4].products << products[6]
+# categories[4].products << products[8]
+
+# categories[5].products << products[3]
+# categories[5].products << products[4]
+# categories[5].products << products[6]
